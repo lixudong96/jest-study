c998d8bb72f7efc45dcb1f9a3c0fb7932987318e
SunLee <303001274@qq.com>
2022-07-27 23:21:23 +0800

    feat: mock的另一种方法

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -1,18 +1,18 @@
-Object.defineProperty(global, 'localStorage', {
-  value: {
-    store: {} as Record<string, string>,
-    setItem(key: string, value: string) {
-      this.store[key] = value
-    },
-    getItem(key: string) {
-      return this.store[key]
-    },
-    removeItem(key: string) {
-      delete this.store[key]
-    },
-    clear() {
-      this.store = {}
-    },
-  },
-  configurable: true,
-})
+// Object.defineProperty(global, 'localStorage', {
+//   value: {
+//     store: {} as Record<string, string>,
+//     setItem(key: string, value: string) {
+//       this.store[key] = value
+//     },
+//     getItem(key: string) {
+//       return this.store[key]
+//     },
+//     removeItem(key: string) {
+//       delete this.store[key]
+//     },
+//     clear() {
+//       this.store = {}
+//     },
+//   },
+//   configurable: true,
+// })
