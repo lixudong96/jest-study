71086f3a8ef1b3e6c61078c7298fa430f36c7ffa
sunlee <303001274@qq.com>
2022-07-28 14:21:21 +0800

    feat: import react and first snapshot test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Title from './components/Title'
+
+const App = () => {
+  return (
+    <div>
+    <section>
+      <Title type="small" title="小字" />
+      <Title type="large" title="大字" />
+    </section>
+  </div>
+  )
+}
+
+export default App
